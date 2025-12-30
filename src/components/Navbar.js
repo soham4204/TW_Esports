da47f11e5f45bfb4c2c6b419ef3c42b8ef558774
soham4204 <psoham104@gmail.com>
2025-12-30 12:11:41 +0530

    navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           {/* Logo / Home Section */}
-          <Link className="flex items-center group" to="/">
+          <Link className="flex items-center group" to="/home">
             <div className="flex items-center space-x-3">
               <div className="relative h-10 w-10 overflow-hidden">
                 <img 
